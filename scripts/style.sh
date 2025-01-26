2ce454c14d238c95dba08f92505b7bac923c204c
darmsDD <ivandinizdobbin2@gmail.com>
2025-01-26 16:22:29 -0300

    Change comment pattern in style.sh

--- scripts/style.sh
+++ scripts/style.sh
@@ -13,8 +13,6 @@
 #
 # =====================================================================
 
-# Este arquivo é resposnsável pelas estilizações, isto é teste em cores
-# Indicar o final de operações e etc.
 
 #Colors for text
 BBlack='\033[1;30m'       # Black
@@ -27,100 +25,110 @@ BCyan='\033[1;36m'        # Cyan
 On_Black='\033[40m'       # Black background
 stage_over_char="="
 
-: '
-    Purpose:    Prints a sentence with the standard color, white.
-    Arguments:  1- The sentence to be printed.
-                2- Additional flag to echo.
-'
+#====================================================================================
+#   Description:    Prints a sentence with the standard color, white.
+#   Arguments:      1- The sentence to be printed.
+#                   2- Additional flag to echo.
+#====================================================================================
 Style_NormalWorld(){
     echo -e $2 ${Color_Off}$1
 }
 
 
-: '
-    Purpose:    Prints a sentence in red. Used when something went wrong.
-    Arguments:  1- The sentence to be printed.
-                2- Additional flag to echo.
-'
+#====================================================================================
+#   Description:    Prints a sentence in red. Used when something went wrong.
+#   Arguments:      1- The sentence to be printed.
+#                   2- Additional flag to echo.
+#====================================================================================
 Style_RedWord () {
     echo -e $2 ${BRed}$1${Color_Off}
 }
 
 
-: '
-    Purpose:    Prints a sentence in black.
-    Arguments:  1- The sentence to be printed.
-                2- Additional flag to echo.
-'
+#====================================================================================
+#   Description:   Prints a sentence in black.
+#   Arguments:     1- The sentence to be printed.
+#                  2- Additional flag to echo.
+#====================================================================================
 Style_BlackWord () {
     echo -e $2 ${BBlack}$1${Color_Off}
 }
 
 
-: '
-    Purpose:    Prints a sentence in green. Used when an operation was successfull.
-    Arguments:  1- The sentence to be printed.
-                2- Additional flag to echo.
-'
+#====================================================================================
+#   Description:   Prints a sentence in green. Used when an operation was 
+#                   successfull.
+#   Arguments:     1- The sentence to be printed.
+#                   2- Additional flag to echo.
+#====================================================================================
 Style_GreenWord () {
     echo -e $2 ${BGreen}$1${Color_Off}
 }
 
 
-: '
-    Purpose:    Prints a sentence in Yellow. Used as an alternative for Style_PurpleWord to print messages.
-    Arguments:  1- The sentence to be printed.
-                2- Additional flag to echo.
-'
+#====================================================================================
+#   Description:   Prints a sentence in Yellow. Used as an alternative for 
+#                   Style_PurpleWord to print messages.
+#
+#   Arguments:     1- The sentence to be printed.
+#                  2- Additional flag to echo.
+#====================================================================================
 Style_YellowWord () {
     echo -e $2 ${BYellow}$1${Color_Off}
 }
 
 
-: '
-    Purpose:    Prints a sentence in purple. Used as an alternative for Style_YellowWord to print messages. 
-    Arguments:  1- The sentence to be printed.
-                2- Additional flag to echo.
-'
+#====================================================================================
+#   Description:    Prints a sentence in purple. Used as an alternative for 
+#                   Style_YellowWord to print messages. 
+#
+#   Arguments:      1- The sentence to be printed.
+#                   2- Additional flag to echo.
+#====================================================================================
 Style_PurpleWord () {
     echo -e $2 ${BPurple}$1${Color_Off}
 }
 
-: '
-    Purpose:    Indicates with echo the end of a stage.
-    Arguments:  None
-'
+#====================================================================================
+#   Description:    Indicates with echo the end of a stage.
+#   Arguments:      None
+#====================================================================================
 Style_StageOver (){
 	echo  -e "${On_Black}$repeated_char${Color_Off}"
 	echo -e "${On_Black}$repeated_char${Color_Off}\n"
 }
 
 
-: '
-    Purpose:    Indicates with echo the beginning of a stage.
-    Arguments:  1- Stage sentence shown to user. Example: "0- Defining the workspace and the project"
-                2- Additional flag to echo.
-'
+#====================================================================================
+#   Description:   Indicates with echo the beginning of a stage.
+#
+#   Arguments:     1- Stage sentence shown to user. Example: 
+#                       "0- Defining the workspace and the project"
+#                  2- Additional flag to echo.
+#====================================================================================
 Style_StageInit(){
     echo -e $2 ${BYellow}$1"\n"${Color_Off} 
 }
 
 
-: '
-    Purpose:    Indicates with echo the beginning of a substage.
-    Arguments:  1- Stage sentence shown to user. Example: "1.2- Defining the project"
-                2- Additional flag to echo.
-'
+#====================================================================================
+#   Description:    Indicates with echo the beginning of a substage.
+#
+#   Arguments:      1- Stage sentence shown to user. 
+#                       Example: "1.2- Defining the project"
+#                   2- Additional flag to echo.
+#====================================================================================
 Style_SubStageInit(){
     echo -e $2 "\t"${BYellow}$1"\n"${Color_Off} 
 }
 
 
-: '
-    Purpose:    Prints a char n times.
-    Arguments:  1- Char to be printed."
-                2- Number of times the char will be printed.
-'
+#====================================================================================
+#   Description:   Prints a char n times.
+#
+#   Arguments:     1- Char to be printed.
+#                  2- Number of times the char will be printed.
+#====================================================================================
 Style_RepeatChar(){
   char=$1
   count=$2
@@ -128,10 +136,10 @@ Style_RepeatChar(){
 }
 
 
-: '
-    Purpose:    Formats a multiline sentence to be used in a sed command.
-    Arguments:  1- Multiline sentence.
-'
+#====================================================================================
+#   Description:    Formats a multiline sentence to be used in a sed command.
+#   Arguments:      1- Multiline sentence.
+#====================================================================================
 Style_FormatMultilineVarForSed(){
     sed_multiline_array="$@"
     formatted_multine=$(echo "$sed_multiline_array" | sed "s/ /\\\\\n/g")
